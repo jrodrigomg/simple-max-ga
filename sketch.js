d1dbf6b45c64c17a154ce4bd41e0b83cc1f57bbb
jrodrigomg <jrodrigomg@hotmail.com>
2019-08-11 22:58:17 -0600

    dynamic restriction and variables

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 //Aim function
-const a_x1 = 1; //First coeficient
-const b_x2 = 4; //Second coeficient
+const z_coeficients= [4,3,2];
+
 
 //Restrictions
-const c_x1 = 4;
-const d_x2 = 3;
-const b1 = 10;
+//The last value is the b, its the right of inequality
+const restrictions = [
+    [1,2,5,60], 
+     [2,1,0,10]
+]
+
 
 //Configurations
 let population = [];//Population array
 let fitness = []; //Fitness array
 //Simplex solver with generic algorithm
-const SIZE_POPULATION = 200; //Population max size
+const SIZE_POPULATION = 10; //Population max size
 const MUATATION_FACTOR = 0.4; //mutation factor
 
 
@@ -24,17 +27,25 @@ let value_best_current = 0;
 
 function setup() 
 {
-    createCanvas(1200,700);
+    createCanvas(1400,700);
     generateFirstPopulation();
+    // calculateFitness();
+    // generateGeneration();
 }
 
 function generateFirstPopulation()
 {
     for(let i=0; i<SIZE_POPULATION;i++)
     {
-        //Creando un genome randomizado de dos variables X1 y X2
-        population[i] = [random(0,10), random(0,10)];
+        //Creando un genome randomizado de dos variables X1...Xn
+        population[i] = initializeGenome();
     }
+}
+
+function initializeGenome()
+{
+    return Array(z_coeficients.length)
+        .fill().map(() => Math.round(Math.random() * 10))
 }
 
 function calculateFitness()
@@ -44,8 +55,8 @@ function calculateFitness()
     for(let i=0; i<SIZE_POPULATION; i++)
     {
 
-        b = calculateRestriction(population[i]);
-        if(b<=b1)
+        let valid = validateRestrictions(population[i]);
+        if(valid)
         {
             let z = calculateZ(population[i]);
             let fit = z/ sum;
@@ -57,7 +68,7 @@ function calculateFitness()
                 if( z > value_best_history )
                 {
 
-                    console.log("if " + z + " < " + value_best_history);
+                    console.log("if " + z + " > " + value_best_history);
                     value_best_history = z;
                     best_history = population[i];
                 }
@@ -75,9 +86,9 @@ function calculateSumFitness()
     let sum = 0;
     for(let i=0; i<SIZE_POPULATION; i++)
     {
-        b = calculateRestriction(population[i]);
+        let valid = validateRestrictions(population[i]);
         //If is up to b1 we don't sum anything
-        if(b<=b1)
+        if(valid)
         {
             sum+=calculateZ(population[i]);
         }
@@ -85,14 +96,35 @@ function calculateSumFitness()
     return sum;
 }
 
-function calculateRestriction(genome)
+function validateRestrictions(genome)
 {
-    return c_x1*genome[0] + d_x2*genome[1];
+    let valid = true;
+    for(let i=0;i<restrictions.length;i++)
+    {
+        valid = valid & validateRestriction(restrictions[i], genome);
+    }
+    return valid;
+}
+
+function validateRestriction(restriction,genome)
+{
+    let b = 0;
+    for(let i=0; i<restriction.length - 1;i++)
+    {
+        b +=restriction[i] * genome[i];
+    }
+    //Return if the restriction is valid with <=
+    return b <= restriction[restriction.length - 1];
 }
 
 function calculateZ(genome)
 {
-    return a_x1*genome[0] + b_x2*genome[1];
+    let sum = 0;
+    for(let i=0; i<genome.length; i++)
+    {
+        sum+= genome[i] * z_coeficients[i];
+    }
+    return sum;
 }
 
 function generateGeneration()
@@ -111,15 +143,29 @@ function generateGeneration()
 
 function crossOver(g1, g2)
 {
-    g_new = [g1[0], g2[1]];
-    return g_new;    
+    new_genome = [];
+    //Calculate the middle
+    let n = z_coeficients.length;
+    let middle = floor(n/2);
+    //Assign the first bunch of coeficient
+    for(let i=0;i<middle;i++)
+    {
+        new_genome[i] = g1[i];
+    }
+    //Assign the second bunch of coeficient
+    for(let i=middle; i<n;i++)
+    {
+        new_genome[i] = g2[i];
+    }
+    return new_genome;    
 }
 
 function mutate(genome)
 {
+    let n = z_coeficients.length;
     if(random(1) < MUATATION_FACTOR)
     {
-        index = int(random(2));
+        index = int(random(n));
         genome[index] = random(0,10);
     }
 }
@@ -129,16 +175,42 @@ function pickOne(list, prob)
 {
     var index = 0;
     var r = random(1);
-  
+    // console.log(list,prob)
     while (r > 0) 
     {
       r = r - prob[index];
       index++;
     }
     index--;
+    // console.log(list[index])
     return list[index].slice();
 }
 
+function getFunctionString(z)
+{
+    let z_function = "Z = ";
+    for (let i=0;i<z.length;i++)
+    {
+        z_function+= z[i] + "*" + "X"+i + " + ";
+    }
+    return z_function;
+}
+
+function getRestrictionTitle()
+{
+    let restriction_title = "With: \n";
+    for(let i =0; i< restrictions.length;i++)
+    {
+        let restriction = restrictions[i];
+        for (let j=0; j < restriction.length - 1; j++)
+        {
+            restriction_title += restriction[j] + "*X" + j + " + ";
+        }
+
+        restriction_title += "<= " + restriction[restriction.length - 1] + "\n"; 
+    }
+    return restriction_title;
+}
 
 function draw() 
 {
@@ -149,20 +221,26 @@ function draw()
 
     fill(255)
     textSize(32);
-    text('Z = ' + a_x1 + "*X1 + " + b_x2 + "*X2 ", width / 3, 30);
-    text('With: ' + c_x1 + "*X1 + " + d_x2 + "*X2 <= " + b1, width/4, 80);
+
+    let z_function = getFunctionString(z_coeficients);
+    let restriction_title = getRestrictionTitle();
+
+    text(z_function, 0, 30);
+    text(restriction_title, 0, 80);
 
     if(value_best_history>0)
     {
          //Show best
-        text("Best History", width/3,110);
-        text("Z = " + a_x1 + "*" + best_history[0] + " + " + b_x2 + "*" + best_history[1] + " = " + value_best_history, 0, 150);
+        text("Best History", 0,400);
+        let best_history_string = getFunctionString(best_history);
+        text(best_history_string, 0, 450);
     }
 
     if(value_best_current>0)
     {
         //Best current
-        text("Best Current", width/3,200);
-        text("Z = " + a_x1 + "*" + best_current[0] + " + " + b_x2 + "*" + best_current[1] + " = " + value_best_history, 0, 250);
+        text("Best Current", 0,500);
+        let best_current_string = getFunctionString(best_current);
+        text(best_current_string, 0, 550);
     }
 }
